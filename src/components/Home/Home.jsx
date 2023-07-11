d14691c94717fec234f02964635fc6472fa2e112
Akash Parmar <ak.parmar1442000@gmail.com>
2023-07-11 16:07:22 +0530

    navBarCreated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,63 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './Home.module.css';
 import Hero from '../Hero/Hero';
 import Aboutme from '../Aboutme/Aboutme';
 import Navbar from '../Navbar/Navbar';
 const Home = () => {
+  const [children, setChildren] = useState([]);
+  const homeRef = useRef({
+    background : '',
+    about : false,
+    project : false,
+    contactMe : false
+  })
+
+  useEffect(()=>{
+    const onScroll = (event)=>{
+
+      const childArr = event?.target.children[0].children[1].children[0].children[0].children;
+      const temp  = [];
+      for(let i=0 ; i<childArr.length; i++){
+           temp.push({
+          clientHeight : childArr[i].clientHeight,
+           clientWidth : childArr[i].clientWidth, 
+           offsetHeight : childArr[i].offsetHeight, 
+           offsetWidth : childArr[i].offsetWidth
+          });
+      }
+      setChildren(temp);
+      // to set Nav Background
+      if(window.scrollY >= (temp[0].clientHeight * 3)){
+        homeRef.current.background = 'rgba(0,0,0,0.5)';
+      }else{
+        homeRef.current.background = 'transparent';
+      }
+
+      //to active navLinks as per the scroll
+      let gaps = [temp[1].clientHeight - 20, temp[1].clientHeight + temp[2].clientHeight - 20];
+      if(window.scrollY > gaps[0] && window.scrollY < gaps[1]){
+        homeRef.current.about = true;
+      }else{
+        homeRef.current.about = false;
+      }
+      
+    };
+
+    window.addEventListener('scroll', onScroll, {passive: true});
+
+    return()=>{
+      window.removeEventListener('scroll', onScroll)
+    }
+  });
 
   return (
-    <section className={styles.home}>
-        <Navbar />
+    <section className={styles.home} ref={homeRef}>
+        <Navbar 
+        color = {homeRef.current.background} 
+        isAbout = {homeRef.current.about}
+        isProject = {homeRef.current.project}
+        isContact = {homeRef.current.contactMe}
+        />
         <Hero />
         <Aboutme />
     </section>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import styles from './Navbar.module.css';
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { color, isAbout, isProject, isContact } = props;
+  const style = {
+    background : color,
+    transition : 'background 250ms ease-in-out'
+  }
   return (
-    <div>Navbar</div>
+    <nav className={styles.nav} style={style}>
+      <div className={styles.brandName}>
+          <span>SKY</span><span>FOLIO</span>
+      </div>
+      <ul className={styles.navUl}>
+        <li className={`${styles.navLi} ${isAbout?styles.liAnim:''}`}>
+            About
+        </li>
+        <li className={`${styles.navLi} ${isProject?styles.liAnim:''}`}>
+            Projects
+        </li>
+        <li className={`${styles.navLi} ${isContact?styles.liAnim:''}`}>
+            Hit me up
+        </li>
+      </ul>
+    </nav>
   )
 }
 
