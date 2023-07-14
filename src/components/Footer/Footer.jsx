09e8f199e61f950affbe4f2b0384a22fb84486a0
Akash Parmar <ak.parmar1442000@gmail.com>
2023-07-14 12:30:40 +0530

    FooterCompletedWithEmailFunctionality

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,69 @@
 import React, { useState } from 'react';
 import styles from './Footer.module.css';
 import logo from '../../assets/svg/skyFolio.png';
+import emailjs from 'emailjs-com'
+
+
 const Footer = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
-  return (
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState("");
+    const errorTimingFun = (e)=>{
+      setTimeout(()=>{setError('')}, 1500);
+    }
+
+    const handleSubmit = (e)=>{
+      e.preventDefault();
+      
+      if(name === '' || /[0-9]/.test(name)){
+        setError('Please Enter valid name');
+        setName('');
+        errorTimingFun();
+        return;
+      }
+
+      const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+
+
+      if(email === '' || !email.match(mailFormat)){
+        setError('Please Enter valid email');
+        setEmail('');
+        errorTimingFun();
+        return;
+      }
+
+      if(message === ''){
+        setError('Enter a message');
+        errorTimingFun();
+        return;
+      }
+
+      console.log('name : ', name)
+      console.log('email : ', email)
+      console.log('message : ', message)
+
+      emailjs.sendForm('service_36ia3fx', 'template_tnfslyn', e.target, 'V8PkcoBQ2MaaWslG3')
+      .then((result) => {
+          setSuccess("I'll get back at you soon");
+          setName('');
+          setEmail('');
+          setMessage('');
+          setTimeout(()=>{
+            setSuccess('');
+          },2000);
+          
+      }, (error) => {
+          setError("Error in sending, use other social handles");
+          setName('');
+          setEmail('');
+          setMessage('');
+          errorTimingFun();
+      });
+
+    }
+      return (
     <footer id='footer' className={styles.footer}>
         <div className={styles.contact}>
           <div className={styles.header}>
@@ -16,7 +74,19 @@ const Footer = () => {
               Got a problem to solve?
             </div>
           </div>
-          <form className={styles.form}>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            {
+              error === ''? null : 
+              <p className={styles.error}>
+                <i className="ri-error-warning-line"></i> &nbsp;{error}
+              </p>
+            }
+            {
+              success === ''? null : 
+              <p className={styles.success}>
+                <i className="ri-mail-send-line"></i> &nbsp;{success}
+              </p>
+            }
             <div className={styles.formDiv}>
               <label className={styles.label} htmlFor='name'>Name</label>
               <input 
@@ -36,7 +106,7 @@ const Footer = () => {
               className={styles.input} 
               type='mail' 
               id='email' 
-              name='mail' 
+              name='email' 
               value={email} 
               onChange={(e)=>setEmail(e.target.value)} 
               />
@@ -50,6 +120,7 @@ const Footer = () => {
               <textarea 
               className={styles.textarea} 
               id='message'
+              name='message'
               value={message}
               onChange={(e)=>setMessage(e.target.value)}
               >
@@ -73,27 +144,27 @@ const Footer = () => {
             <ul className={styles.socialUl}>
               <li className={styles.socialLi}>
                 <a href='https://twitter.com/parmarSKY_' target='_blank'>
-                  <i class="ri-twitter-line"></i>
+                  <i className="ri-twitter-line"></i>
                 </a>
                 </li>
               <li className={styles.socialLi}>
                 <a href='https://www.linkedin.com/in/akash-parmar-/' target='_blank'>
-                  <i class="ri-linkedin-line"></i>
+                  <i className="ri-linkedin-line"></i>
                 </a>
                 </li>
               <li className={styles.socialLi}>
                 <a href='http://discordapp.com/users/558636559755247616' target='_blank'>
-                  <i class="ri-discord-line"></i>
+                  <i className="ri-discord-line"></i>
                 </a>
                 </li>
               <li className={styles.socialLi}>
                 <a href='https://github.com/Akash-cloud001' target='_blank'>
-                  <i class="ri-github-line"></i>
+                  <i className="ri-github-line"></i>
                 </a>
                 </li>
             </ul>
             <div className={styles.benchmark}>
-              Handcrafted by me <i class="ri-copyright-line"></i> 2023
+              Handcrafted by me <i className="ri-copyright-line"></i> 2023
             </div>
         </div>
     </footer>
